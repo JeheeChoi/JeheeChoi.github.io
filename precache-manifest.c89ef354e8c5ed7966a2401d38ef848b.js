56f89dc2a0235315282ee539b2145fd4fcfc771e
JeheeChoi <writetojehee@gmail.com>
2020-12-12 21:30:17 -0800

    Initial commit

---
+++ precache-manifest.c89ef354e8c5ed7966a2401d38ef848b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c20e2880e0d96b201a8845fbcaa3a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7a4575efbc071e6bdc05",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4575efbc071e6bdc05",
+    "url": "/static/js/main.20f4687f.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/static/media/resume.0c246fa0.pdf"
+  }
+]);
