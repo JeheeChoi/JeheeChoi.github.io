2ed55dd2b7fa6614b4d07b110d9d638240dc6b7f
JeheeChoi <writetojehee@gmail.com>
2020-12-15 14:10:54 -0800

    trying to revert to previous commit

---
+++ pages.js
@@ -0,0 +1,15 @@
+const ghpages = require("gh-pages");
+const pathname = `${__dirname}/build`;
+const repoURL = "https://github.com/JeheeChoi/JeheeChoi.github.io";
+
+ghpages.publish(
+  pathname,
+  {
+    branch: "main",
+    repo: repoURL,
+  },
+  (err) => {
+    if (err) console.log("ERROR: ", err);
+    else console.log("PUBLISHED");
+  }
+);
